2eb8e90d12aed7fc42dfdd51871b8a477308f4be
steve marlow <kione83@gmail.com>
2025-06-26 15:51:42 -0400

    changes

--- player.py
+++ player.py
@@ -28,6 +28,6 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            pass
+            self.rotate(-dt)
         if keys[pygame.K_d]:
-            pass
+            self.rotate(dt)
